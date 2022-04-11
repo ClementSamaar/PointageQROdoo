b1a79a2f6fd1166d0de5365b6714e6b0d539de8b
ClementSamaar <clement.samaar@etu.univ-amu.fr>
2022-04-11 17:07:49 +0400

    V1 : Design

--- app/src/main/java/rdtronic/csamaar/geoloc/MainActivity.java
+++ app/src/main/java/rdtronic/csamaar/geoloc/MainActivity.java
@@ -2,13 +2,24 @@ package rdtronic.csamaar.geoloc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView longCoordinate = findViewById(R.id.longCoordinate);
+    TextView latCoordinate = findViewById(R.id.latCoordinate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Intent editIntent = new Intent();
+        editIntent.setAction(Intent.ACTION_INSERT_OR_EDIT);
+        editIntent.setType("text/plain");
+        editIntent.putExtra("long", "");
+        editIntent.putExtra("lat", "");
+        String longValue = editIntent;*/
     }
 }
