91ec2195ab245334868900184f7960f3c1fcf46f
ClementSamaar <clement.samaar@etu.univ-amu.fr>
2022-04-13 15:43:34 +0400

    V1.2 : Coordonnées GPS avec LocationCallback appelé une seule fois

--- app/src/main/java/rdtronic/csamaar/geoloc/MainActivity.java
+++ app/src/main/java/rdtronic/csamaar/geoloc/MainActivity.java
@@ -1,49 +1,39 @@
 package rdtronic.csamaar.geoloc;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.annotation.SuppressLint;
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private final Intent editIntent = new Intent();
-    private FusedLocationProviderClient fusedLocationClient;
-    private Location location;
-
-    String[] permissionList = new String[] {
+    final String[] permissionList = new String[] {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
 
-    LocationListener locationListener;
-    LocationManager locationManager;
-
     private TextView coordinatesLabel, longLabel, latLabel, longCoordinate, latCoordinate;
     private Button permReqButton;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private String latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,57 +45,61 @@ public class MainActivity extends AppCompatActivity {
         latLabel = findViewById(R.id.latLabel);
         longCoordinate = findViewById(R.id.longCoordinate);
         latCoordinate = findViewById(R.id.latCoordinate);
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         permReqButton = findViewById(R.id.permissionButton);
-        permReqButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                requestLocationPermission();
-            }
-        });
-
-        editIntent.setAction(Intent.ACTION_INSERT_OR_EDIT);
-        editIntent.setType("");
-
+        permReqButton.setOnClickListener(view -> requestLocationPermission());
         display();
-
-        /*fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location == null) {
-                    Log.v("erreur", "no");
-                } else {
-                    Log.v("long", String.valueOf(location.getLongitude()));
-                    Log.v("lat", String.valueOf(location.getLatitude()));
-                    longCoordinate.setText(String.valueOf(location.getLongitude()));
-                    latCoordinate.setText(String.valueOf(location.getLatitude()));
-                }
-            }
-        });*/
-
-        /*locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                editIntent.putExtra("long", String.valueOf(location.getLongitude()));
-                editIntent.putExtra("lat", String.valueOf(location.getLatitude()));
-                Log.v("long", String.valueOf(location.getLongitude()));
-            }
-        };*/
-
     }
 
-    private void display(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getLocation();
-            coordinatesLabel.setText(R.string.coordinatesLabel_onGranted);
-            if (longLabel.getVisibility() == View.INVISIBLE) longLabel.setVisibility(View.VISIBLE);
-            if (latLabel.getVisibility() == View.INVISIBLE) latLabel.setVisibility(View.VISIBLE);
-            if (permReqButton.getVisibility() == View.VISIBLE) permReqButton.setVisibility(View.INVISIBLE);
+    private final LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+            for (Location location : locationResult.getLocations()) {
+                if (location != null) {
+                    longitude = String.valueOf(location.getLongitude());
+                    latitude = String.valueOf(location.getLatitude());
+                    Log.v("LONGITUDE", String.valueOf(longitude));
+                    Log.v("LATITUDE", String.valueOf(latitude));
+                    display();
+                    if (longitude != null && latitude != null) fusedLocationClient.removeLocationUpdates(locationCallback);
+                }
+            }
         }
-        else {
+    };
+
+    @SuppressWarnings({"deprecation", "ConstantConditions"})
+    @SuppressLint("MissingPermission")
+    private void getLocalisation(){
+        if (fusedLocationClient == null) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(5000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+    }
+
+
+    private void display(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            getLocalisation();
+            if (longitude == null && latitude == null){
+                coordinatesLabel.setText(R.string.coordinatesLabel_onWait);
+                if (longLabel.getVisibility() == View.VISIBLE) longLabel.setVisibility(View.INVISIBLE);
+                if (latLabel.getVisibility() == View.VISIBLE) latLabel.setVisibility(View.INVISIBLE);
+            }
+            else{
+                longCoordinate.setText(longitude);
+                latCoordinate.setText(latitude);
+                coordinatesLabel.setText(R.string.coordinatesLabel_onGranted);
+                if (longLabel.getVisibility() == View.INVISIBLE) longLabel.setVisibility(View.VISIBLE);
+                if (latLabel.getVisibility() == View.INVISIBLE) latLabel.setVisibility(View.VISIBLE);
+            }
+            permReqButton.setVisibility(View.INVISIBLE);
+        }
+        else{
             coordinatesLabel.setText(R.string.coordinatesLabel_onDenied);
             if (longLabel.getVisibility() == View.VISIBLE) longLabel.setVisibility(View.INVISIBLE);
             if (latLabel.getVisibility() == View.VISIBLE) latLabel.setVisibility(View.INVISIBLE);
@@ -113,40 +107,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private void getLocation () {
-        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    Log.v("long", String.valueOf(location.getLongitude()));
-                    longCoordinate.setText(String.valueOf(location.getLongitude()));
-                    latCoordinate.setText(String.valueOf(location.getLatitude()));
-                }
-                else{
-                    Log.v("Erreur", "Location == null");
-                }
-            }
-        });
-    }
 
     public void requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            new AlertDialog.Builder(this).setTitle("Autorisation necessaire")
-                    .setMessage("GeoLoc à besoin d'accéder à votre position afin de pouvoir fonctionner.")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            ActivityCompat.requestPermissions(MainActivity.this, permissionList, 0);
-                        }
-                    })
-                    .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.dismiss();
-                        }
-                    })
+            new AlertDialog.Builder(this).setTitle(R.string.permRationale_title)
+                    .setMessage(R.string.permRationale_msg)
+                    .setPositiveButton(R.string.permRationale_posBtn, (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this, permissionList, 0))
+                    .setNegativeButton(R.string.permRationale_negBtn, (dialogInterface, i) -> dialogInterface.dismiss())
                     .create()
                     .show();
         }
@@ -162,11 +130,10 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED
                     && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this, "GeoLoc utilise votre position", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.permissionGranted_toastText, Toast.LENGTH_SHORT).show();
                 display();
-                //locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 5000, 10, locationListener);
             } else {
-                Toast.makeText(MainActivity.this, "L'application n'utilisera pas votre position", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.permissionDenied_toastText, Toast.LENGTH_SHORT).show();
             }
         }
     }
